b01bc3154fa4aaa7e7d39f2153c42f733850c83a
Abdiel <abdielambriz2402@gmail.com>
2025-11-10 22:13:28 -0600

    :/

---
+++ Galatasaray/Views/SensorsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Galatasaray.Services;
+using Galatasaray.ViewModels;
+
+namespace Galatasaray.Views
+{
+    public partial class SensorsPage : ContentPage
+    {
+        public SensorsPage()
+        {
+            InitializeComponent();
+            BindingContext = new SensorViewModel(new SensorsService());
+
+        }
+    }
+}
